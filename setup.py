22d7b1e56b4d8fa200dd77fc67ae8514a29470c6
David Vierra <codewarrior@hawaii.rr.com>
2016-09-10 13:15:50 -1000

    Initial Commit (PySide 1.2.4)

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pysideuic',
+      version='1.0',
+      description='PySide UI Compiler',
+      author='PySide development team',
+      author_email='do_not_contact@example.com',
+      url='https://www.github.com/mcedit/pysideuic-pkg',
+      packages=find_packages(),
+     )
